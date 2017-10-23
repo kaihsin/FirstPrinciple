f13b218292c4bfff9148c40221e708ee332a9b0d
kaywu <kaywu@localhost.localdomain>
2017-10-23 13:53:50 +0800

    add hw3 tex and Al for 1-1~1-3

--- hw3/src/Hands-on-1016/Al/plot.py
+++ hw3/src/Hands-on-1016/Al/plot.py
@@ -7,7 +7,7 @@ import numpy as np
 if len(sys.argv) < 2 :
     print ("ERROR ev.dat") 
     exit(1)
-
+ID = sys.argv[2]
 f = open(sys.argv[1],'r')
 lines = f.readlines()
 dat = []
@@ -30,20 +30,52 @@ print ("a0_min : ",a0_min)
 ## get Bulk modulus :
 V = dat[:,0]**3
 E = dat[:,1]
+x2 = np.arange(V[0],V[-1],0.0001)
 coef_2 = np.polyfit(V,E,3)
-print coef_2
+print (coef_2)
 V_min = (-2*coef_2[1] + np.sqrt(4*coef_2[1]**2-12*coef_2[0]*coef_2[2]) )/(6*coef_2[0]) 
 #print V_min
 B = (6*coef_2[0]*V_min+2*coef_2[1])*V_min # unit = eV/A^3
-print B
+print (B)
 B = B * 1.6E+11 # J/m^3 = Pa
 B = B * 10**-9
 print ("B : ",B,"GPar")
 
+plt.figure(1)
+# E-a0 
+#----------------------------------------------------------
 plt.plot(dat[:,0],dat[:,1],'x',label='simulate')
 plt.plot(x,np.polyval(coef,x),'-',label = 'fit')
 plt.xlabel('a0')
 plt.ylabel('E')
 plt.grid()
 plt.legend()
+plt.savefig("%s_E-a0.png"%(ID),format="png",dpi=100,transparent=True)
+
+plt.figure(2)
+# E-V
+#----------------------------------------------------------
+plt.plot(V,E,'x',label='simulate')
+plt.plot(x2,np.polyval(coef_2,x2),'-',label = 'fit')
+plt.xlabel('V')
+plt.ylabel('E')
+plt.grid()
+plt.legend()
+plt.savefig("%s_E-V.png"%(ID),format="png",dpi=100,transparent=True)
+
+
+
+
+f = open("%s.res"%(ID),"w")
+f.write("a0_min : %f\n"%(a0_min))
+f.write("B : %f GPar\n"%(B))
+f.write("[E-a0] Coef(a3.a2.a1.a0) = ")
+for c in coef:
+	f.write("%5.8f "%(c))
+f.write('\n')
+f.write("[E-V ] Coef(a3.a2.a1.a0) = ")
+for c in coef_2:
+    f.write("%5.8f "%(c))
+f.write('\n')
+f.close()
 plt.show()
