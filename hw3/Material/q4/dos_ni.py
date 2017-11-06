2ecf989668e6963bcae554c647a97303ef78be55
kaywu <kaywu@localhost.localdomain>
2017-11-06 11:57:32 +0800

    finished hw3

--- hw3/Material/q4/dos_ni.py
+++ hw3/Material/q4/dos_ni.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 paths = []
 tit   = []
-paths.append("nioaf2.0.u.dat")
-tit.append("NiO-AFII dos")
-#paths.append("nioaf2.0.d.dat")
-#tit.append("NiO-AFII spin-down")
+paths.append("nioaf2.1.u.dat")
+tit.append("NiO-AFII spin-up")
+paths.append("nioaf2.1.d.dat")
+tit.append("NiO-AFII spin-down")
 
 dat = []
 lbl = []
@@ -30,15 +30,15 @@ for p in paths :
 
 for j in range(len(paths)):
 	plt.figure(j)
-	for l in range(1):
+	for l in range(len(dat[j][0])-1):
 		plt.plot(dat[j][:,0],dat[j][:,l+1],label=lbl[j][l+1])
 	plt.title(tit[j])
-	plt.axvline(0,0,6,label='Ef',color='0.6',ls='--')
+	plt.axvline(0,0,1.25,label='Ef')
 	plt.xlim([-8,2.5])
-	plt.ylim([0,6])
+	plt.ylim([0,1.25])
 	plt.legend()
-	plt.savefig('NiO-totdos.png',format='png',dpi=100,transparent=True)
-"""
+
+
 plt.figure(5,[10,8])
 for l in range(len(dat[0][0])-1):
 	plt.plot(np.hstack((dat[0][:,0],dat[1][:,0])),np.hstack((dat[0][:,l+1],-dat[1][:,l+1])),label=lbl[j][l+1])
@@ -55,7 +55,6 @@ plt.text(-6,3,"spin-up")
 plt.text(-6,-3,"spin-dwn")
 #plt.grid()
 plt.savefig('NiO-AFII_Ni.png',format='png',transparent=True,dpi=100)
-"""
 plt.show()
 
 
